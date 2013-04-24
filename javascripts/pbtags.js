5cd1223a46d4a4551557ccbcdfadbf10e46ef1bd
Corey Pein <coreypein@caa.columbia.edu>
2013-04-24 15:55:22 +0200

    Update pbtags.js

--- javascripts/pbtags.js
+++ javascripts/pbtags.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function hidepbpg() {
     $( '.pin-tag').html(function( index, oldHtml ) {
         if(this.outerHTML == '<a class="pin-tag" href="http://pinboard.in/u:warisbusiness/t:pg">pg</a>') {
             return '<a class="pin-tag masked" href="/"> </a>'
