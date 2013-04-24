0983afcebc8b88096c2701e1ec59762f63c6daed
Corey Pein <coreypein@caa.columbia.edu>
2013-04-24 15:44:56 +0200

    Update pbtags.js

--- javascripts/pbtags.js
+++ javascripts/pbtags.js
@@ -1,7 +1,7 @@
 $(function() {
     $( '.pin-tag').html(function( index, oldHtml ) {
         if(this.outerHTML == '<a class="pin-tag" href="http://pinboard.in/u:warisbusiness/t:pg">pg</a>') {
-            return '<a class="pin-tag-masked" href="http://pinboard.in/u:warisbusiness/t:pg"><strong>propaganda</strong></a>'
+            return '<a class="pin-tag masked" href="http://pinboard.in/u:warisbusiness/t:pg"><strong>propaganda</strong></a>'
         }
     });
 });
